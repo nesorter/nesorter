28cb123903b973a1975b9603f19b4b7c4ed20dd6
kugimiya <bp.001@yandex.ru>
2022-07-31 16:56:32 +0400

    [+] Добавлена проверка system-wide зависимостей

---
+++ srv/checkSystemDeps.ts
@@ -0,0 +1,16 @@
+import process from 'process';
+
+const isbin = require('isbin');
+let code = 0;
+
+if (!isbin('mpv')) {
+  console.log('!!! mpv is not installed! Installation process will be aborted');
+  code = 1;
+}
+
+if (!isbin('ffmpeg')) {
+  console.log('!!! ffmpeg is not installed! Installation process will be aborted');
+  code = 1;
+}
+
+process.exitCode = code;
